4b569354dfaaeaab9e5802d49455ce60094528fa
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-03-28 14:29:39 +0200

    choose filter theme

--- src/routes/choose/+page.svelte
+++ src/routes/choose/+page.svelte
@@ -2,8 +2,11 @@
 	import ImageButton from '$lib/ImageButton.svelte';
 	import type { TImage } from '$lib/images';
 	import { selectedImages } from '../../stores';
+	import type { TCategorie } from '$lib/images';
 
-	export let data: { images: TImage[] };
+	export let data: { images: TImage[]; categories: TCategorie[] };
+
+	const { images, categories } = data;
 
 	function setSelectedImages(image: TImage) {
 		const index = $selectedImages.findIndex((img) => img.src === image.src);
@@ -20,7 +23,6 @@
 			]);
 		}
 	}
-	const { images } = data;
 	//console.log({ data });
 </script>
 
@@ -28,7 +30,24 @@
 
 <div class="grid gap-4 grid-cols-3 grid-rows-3 mx-6">
 	{#each images as image}
-		<ImageButton {image} onClick={setSelectedImages} />
+		{#if images.categorie == 'voiture'}
+			<section>
+				<h3>voiture</h3>
+				<ImageButton {image} onClick={setSelectedImages} />
+			</section>
+		{/if}
+		{#if images.categorie == 'chat'}
+			<section>
+				<h3>Chat</h3>
+				<ImageButton {image} onClick={setSelectedImages} />
+			</section>
+		{/if}
+		{#if images.categorie == 'vache'}
+			<section>
+				<h3>Chien</h3>
+				<ImageButton {image} onClick={setSelectedImages} />
+			</section>
+		{/if}
 	{/each}
 
 	<a href="/play" class="p-4 bg-gray-200 rounded-md">Start a game !</a>
