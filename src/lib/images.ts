71c0913f2067a75e41567d94c65eba84bc5e1a18
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-28 10:56:14 +0200

    add image on lib

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -9,7 +9,33 @@ export const images: TImage[] = [
   {
     src: '/images/nom-de-l-image.png',
     alt: 'image de chat',
-    categorie: 'chat'}
+    categorie: 'chat'
+  },
+  {
+    src: '/images/chat1.png',
+    alt: 'image de chat',
+    categorie:'chat'
+  },
+  {
+    src: '/images/chat2.png',
+    alt: 'image de chat',
+    categorie:'chat'
+  },
+  {
+    src: '/images/vache1.png',
+    alt: 'image de vache',
+    categorie:'vache'
+  },
+  {
+    src: '/images/vache2.png',
+    alt: 'image de vache',
+    categorie:'vache'
+  },
+  {
+    src: '/images/vache3.png',
+    alt: 'image de vache',
+    categorie:'vache'
+  },
 
 ]
 
