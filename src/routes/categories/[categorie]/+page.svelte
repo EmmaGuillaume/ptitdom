d88fced488b3c6d11b066ae62c80dbcfd543ccd6
louis lasserre <louis.lasserre@mmibordeaux.com>
2023-03-28 17:11:58 +0200

    changed layout

--- src/routes/categories/[categorie]/+page.svelte
+++ src/routes/categories/[categorie]/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import ImageButton from '$lib/ImageButton.svelte';
 	import type { TCategorie } from '$lib/images';
 	import { categories } from '$lib/images.ts';
 	export let data;
 	console.log(data);
 	const { categorieData } = data as { categorieData: TCategorie };
+
+	const setSelectedImages = () => {};
 </script>
 
-{#each categorieData.images as categorie}
-	<img src={categorie.src} alt="" srcset="" />
-{/each}
+<div class="grid gap-4 grid-cols-3 grid-rows-2 mx-20 mb-10">
+	{#each categorieData.images as categorie}
+		<ImageButton src={categorie.src} onClick={setSelectedImages} />
+		<!-- <img src={categorie.src} alt="" srcset="" /> -->
+	{/each}
+</div>
