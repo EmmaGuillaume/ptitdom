60be75fced0fb707594825e032b2469b16c87aa8
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-03-28 11:00:34 +0200

    images voitures

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -9,13 +9,29 @@ export const images: TImage[] = [
   {
     src: '/images/nom-de-l-image.png',
     alt: 'image de chat',
-    categorie: 'chat'}
-
+    categorie: 'chat'
+  },
+  {
+    src: '/images/voiture.jpg',
+    alt: 'image de voiture1',
+    categorie: 'voiture'
+  },
+  {
+    src: '/images/voiture.jpg',
+    alt: 'image de voiture2',
+    categorie: 'voiture'
+  },
+  {
+    src: '/images/voiture.jpg',
+    alt: 'image de voiture3',
+    categorie: 'voiture'
+  }
 ]
 
 export type TCategorie = string
 
 export const categories: TCategorie[] = [
   "chat",
-  "chien"
+  "chien",
+  "voiture"
 ]
