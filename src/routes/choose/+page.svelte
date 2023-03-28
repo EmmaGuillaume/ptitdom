dc32b38977f38751cd0c349db91aa9db28503dd7
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-28 15:42:27 +0200

    correction button play game

--- src/routes/choose/+page.svelte
+++ src/routes/choose/+page.svelte
@@ -41,7 +41,7 @@
 	>
 	<a
 		href="/play"
-		class="mb-9 flex items-center justify-center h-12 w-72 p-4 text-center rounded-2xl border-solid border-2 font-mono font-bold text-xl block mx-auto"
-		>Start a game !
+		class="bg-green mb-9 flex items-center justify-center h-12 w-72 p-4 text-center rounded-2xl font-mono font-bold text-xl block mx-auto"
+		>Commencer le jeu
 	</a>
 </div>
