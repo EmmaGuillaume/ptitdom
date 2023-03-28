55b5a5d8290851db2fb0cb27948a000678025ec3
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-28 20:53:18 +0200

    Fonction del begin in my-image

--- src/routes/my-images/+page.svelte
+++ src/routes/my-images/+page.svelte
@@ -3,16 +3,31 @@
 
 	const images = $selectedImages;
 	$: countImg = $selectedImages.length;
+
+	function del(img) {
+		alert('delete');
+	}
 </script>
 
 <h2 class="decoration-black text-3xl font-mono font-bold mt-28 mx-16">Les cartes choisies</h2>
 <div class="grid gap-5 grid-cols-3 grid-rows-2 mx-20 mb-10">
 	{#each images as image}
-		<img class="object-cover w-full aspect-square rounded-3xl" src={image.src} alt="" srcset="" />
+		<img
+			on:click={del}
+			class="object-cover w-full aspect-square rounded-3xl"
+			src={image.src}
+			alt={image.name}
+			srcset=""
+		/>
 	{/each}
 </div>
 {#if countImg == 0}
 	<p class="text-xl font-mono mx-28 mt-12 mb-36">Vous n'avez pas encore selectionné d'images :(</p>
+	<style>
+		.startgame {
+			display: none;
+		}
+	</style>
 {/if}
 <div class="flex flex-row">
 	<a
@@ -22,7 +37,7 @@
 	>
 	<a
 		href="/play"
-		class="bg-green mb-9 flex items-center justify-center h-12 w-72 p-4 text-center rounded-2xl font-mono font-bold text-xl block mx-auto"
+		class="startgame bg-green mb-9 flex items-center justify-center h-12 w-72 p-4 text-center rounded-2xl font-mono font-bold text-xl block mx-auto"
 		>Commencer le jeu
 	</a>
 </div>
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -7,7 +7,7 @@
 <div class="grid gap-4 grid-cols-3 mx-6 mt-9 mb-10">
 	{#each images as image}
 		<div class="w-full aspect-square">
-			<img class="w-full h-full object-cover" src={image.src} alt="" srcset="" />
+			<img class="w-full h-full object-cover rounded-3xl" src={image.src} alt="" srcset="" />
 		</div>
 	{/each}
 </div>
