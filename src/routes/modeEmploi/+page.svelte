c94f3bebf8a8d15a56cd0961205fba1c4709eccc
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-28 12:30:13 +0200

    add page modeEmploi

---
+++ src/routes/modeEmploi/+page.svelte
@@ -0,0 +1,21 @@
+<h1 class="decoration-black text-3xl font-mono font-bold mt-28 mx-16">Mode d’emploi :</h1>
+<p class="text-xl font-mono mx-28 mt-12">
+	Voici le mode d’emploi du site. <br />
+
+	Premièrement, sélectionnez les images que vous voulez pour pouvoir faire le jeu des images. Vous
+	pouvez aussi en ajouter avec le bouton ajouter une image. Vous pouvez en sélectionner jusqu’à 100.
+	<br /> <br />
+
+	Une fois vos images sélectionnées, vous pourrez ensuite sauvegarder votre dossier d’images en
+	cliquant sur le bouton de sauvegarde afin de pouvoir réutiliser votre configuration pour les
+	prochaines fois. <br />
+	(Vous pouvez aussi cliquer dans votre panier et supprimer des images sélectionnées pour en ajouter
+	d’autres si ces dernières ne vous plaisent pas.) <br /> <br />
+
+	Une fois que vous avez vos images, cliquez sur commencer l’exercice pour démarrer le jeu. <br />
+	<br />
+
+	<strong class="font bold">Avertissement</strong> : Nous vous conseillons de garder votre code
+	confidentiel en privé afin de pouvoir retrouver vos images sélectionnés quand vous vous
+	reconnecterez sur le site. <br /> <br />
+</p>
