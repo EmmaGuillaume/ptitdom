c704ae59a202313950ba2e142808c10f2bd78d24
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-28 12:13:53 +0200

    style main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 </script>
 
-<h1 class="text-blue-500 underline">Ptit Dom Qizz</h1>
+<h1 class="decoration-black text-3xl font-mono font-bold text-center mt-24">
+	Bienvenue sur le jeu des images
+</h1>
+<p class="text-center text-xl mt-3">
+	Basée sur l’approche ABA, le jeu des images sert pour faciliter l’apprentissage du vocabulaire.
+</p>
+<div class="justify-center flex mt-4">
+	<img src="../../images/vache2.jpg" alt="" />
+</div>
 
-<section>
-	<img src=".../static/images" alt="" />
-	<img src="" alt="" />
-	<img src="" alt="" />
-	<img src="" alt="" />
-</section>
-
-<a href="/chooseTheme" class="underline">Questions</a>
-<a href="/choose" class="p-4 bg-gray-200 rounded-md">Start a game !</a>
+<div class="justify-center items-center flex-col flex gap-7 mt-7">
+	<a
+		href="/choose"
+		class="w-72 p-4 text-center bg-gray-800 text-neutral-50 font-mono font-bold text-xl"
+		>Commencer la sélection
+	</a>
+	<a
+		href="/choose"
+		class="w-72 p-4 text-center bg-gray-800 text-neutral-50 font-mono font-bold text-xl"
+		>Mode d’emploi
+	</a>
+</div>
