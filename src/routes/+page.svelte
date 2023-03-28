8974612f642fc015534e50755bbec18f18062f8a
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-28 15:47:16 +0200

    main img change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	Basée sur l’approche ABA, le jeu des images sert pour faciliter l’apprentissage du vocabulaire.
 </p>
 <div class="justify-center flex mt-4">
-	<img src="../../images/vache2.jpg" alt="" />
+	<img src="../../images/mainimg.png" alt="" />
 </div>
 
 <div class="justify-center items-center flex-col flex gap-7 mt-7">
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -4,7 +4,7 @@
 	const images = $selectedImages;
 </script>
 
-<div class="grid gap-4 grid-cols-3 grid-rows-3 mx-6 mt-9">
+<div class="grid gap-4 grid-cols-3 mx-6 mt-9 mb-10">
 	{#each images as image}
 		<div class="w-full aspect-square">
 			<img class="w-full h-full object-cover" src={image.src} alt="" srcset="" />
