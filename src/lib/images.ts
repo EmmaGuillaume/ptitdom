44c288901e63368ed39d3e88bb4742b01a6bf8a9
louis lasserre <louis.lasserre@mmibordeaux.com>
2023-03-28 10:52:11 +0200

    lib/images

---
+++ src/lib/images.ts
@@ -0,0 +1,21 @@
+
+export type TImage = {
+  src: string,
+  alt: string,
+  categorie: string
+}
+
+export const images: TImage[] = [
+  {
+    src: '/images/nom-de-l-image.png',
+    alt: 'image de chat',
+    categorie: 'chat'}
+
+]
+
+export type TCategorie = string
+
+export const categories: TCategorie[] = [
+  "chat",
+  "chien"
+]
