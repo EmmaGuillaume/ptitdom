044165fd804ae0a617b394ca08c439ee51eddfe3
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-30 18:02:17 +0200

    PLAY PAGE style

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -3,16 +3,15 @@
 	import { selectedImages } from '../../stores';
 
 	const images = $selectedImages;
-
 </script>
 
-<div class="grid gap-4 grid-cols-2 mx-6 mt-9 mb-10 md:grid-cols-3 xl:grid-cols-5">
-	{#each images as image}
-		<div class="w-full aspect-square">
-			<img class="w-full h-full object-cover rounded-3xl" src={image.src} alt="" srcset="" />
-		</div>
-	{/each}
-</div>
-
-<BackButton href="/my-images" />
-
+<main class="p-16 h-[100vh]">
+	<BackButton href="/my-images" />
+	<div class="grid gap-4 grid-cols-2 mt-9 mb-10 md:grid-cols-3 xl:grid-cols-5">
+		{#each images as image}
+			<div class="w-full aspect-square">
+				<img class="w-full h-full object-cover rounded-3xl" src={image.src} alt="" srcset="" />
+			</div>
+		{/each}
+	</div>
+</main>
