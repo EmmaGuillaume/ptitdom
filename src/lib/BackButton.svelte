709b1b3f29b6cd027202c24da9bbd03fdce98051
Héloise <heloise.pingitore@mmibordeaux.com>
2023-03-30 16:00:32 +0200

    witdh correct

--- src/lib/BackButton.svelte
+++ src/lib/BackButton.svelte
@@ -4,7 +4,7 @@
 
 <a
 	{href}
-	class="flex items-center justify-center gap-8 h-12 w-[345px] p-4 text-center rounded-2xl font-mono font-bold text-xl bg-blue"
+	class="flex items-center justify-center gap-8 h-12 w-56 p-4 text-center rounded-2xl font-mono font-bold text-xl bg-blue"
 >
 	<img src="/images/icons/back-arrow.png" alt="" class="w-7" /> Retour
 </a>
