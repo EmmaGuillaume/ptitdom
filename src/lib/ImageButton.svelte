4093573d8e2f1aed0f74d72ded4cf791850243b1
louis lasserre <louis.lasserre@mmibordeaux.com>
2023-03-30 17:18:11 +0200

    check

--- src/lib/ImageButton.svelte
+++ src/lib/ImageButton.svelte
@@ -22,9 +22,18 @@
 
 <button
 	on:click={() => handleClick(image)}
-	class={`w-full h-full border-8 border-gray rounded overflow-hidden ${
-		active === true ? 'border-green' : ''
+	class={`w-full h-full border-8 border-gray relative rounded overflow-hidden ${
+		active === true ? 'border-blue' : ''
 	} `}
 >
+	<div
+		class={`check rounded-full w-7 aspect-square absolute grid place-items-center border-2 right-2 top-2 ${
+			active === true ? 'bg-blue border-blue ' : 'border-gray'
+		} `}
+	>
+		{#if active}
+			<img class="w-4 h-4" src="/images/check.svg" alt="" />
+		{/if}
+	</div>
 	<img class="object-cover w-full h-full aspect-square" src={image.src} alt="alt" srcset="" />
 </button>
