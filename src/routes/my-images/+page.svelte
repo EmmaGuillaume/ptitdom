f507cb566f1e8093bdb4ca4b5c2e4d0882431031
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-03-30 09:59:16 +0200

    stores test

--- src/routes/my-images/+page.svelte
+++ src/routes/my-images/+page.svelte
@@ -3,11 +3,13 @@
 	import BackButton from '$lib/BackButton.svelte';
 	import ImageButton from '$lib/ImageButton.svelte';
 	import ImgDel from '$lib/ImgDel.svelte';
-	import { selectedImages, validation } from '../../stores';
-
-	const images = localStorage.getItem('selectedImages') || '';
+	import { stored, selectedImages } from '../../stores';
+	console.log(stored);
+	let images = localStorage.getItem('selectedImages') || '';
 	// const images = $selectedImages;
 	$: countImg = $selectedImages.length;
+
+	console.log(stored);
 </script>
 
 <h2 class="decoration-black text-3xl font-mono font-bold mt-20 mx-16 mb-16">Les cartes choisies</h2>
--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,9 @@
 import type { TImage } from '$lib/images';
 import { writable } from 'svelte/store'
 
+export const stored = localStorage.content;
+export const content = writable(stored || 'Hello, World!')
+content.subscribe((value) => localStorage.content = value)
 
 export const selectedImages = writable<TImage[] | []>([]);
 localStorage.setItem('selectedImages', selectedImages);
