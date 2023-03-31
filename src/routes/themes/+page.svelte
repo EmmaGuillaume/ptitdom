51e557e0b898d1d769f63a12e985c367d00f55c3
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-03-31 10:25:31 +0200

    add meta tags SEO -- template arthaud proust https://metatags.arthaud.dev/

--- src/routes/themes/+page.svelte
+++ src/routes/themes/+page.svelte
@@ -50,6 +50,55 @@
 		};
 	}); */
 </script>
+<svelte:head>
+	<title>Bild, votre jeu de cartes numérique - Bild</title>
+
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<meta name="robots" content="all" />
+	<meta name="target" content="all" />
+
+	<meta name="author" content="Bild" />
+	<meta name="owner" content="Bild" />
+	<meta name="language" content="fr" />
+	<meta http-equiv="content-language" content="fr" />
+
+	<meta name="url" content="https://ptitdom.vercel.app/" />
+	<meta name="identifier-URL" content="https://ptitdom.vercel.app/" />
+	<link rel="canonical" href="https://ptitdom.vercel.app/themes" />
+
+	<meta name="subject" content="Identification d'images" />
+	<meta
+		name="description"
+		content="Bild est un outil pédagogique permettant aux enfants ayant des troubles du spectre autistique d'identifier différents objets ou situations "
+	/>
+
+	<meta property="og:title" content="Bild, votre jeu de cartes numérique" />
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:description"
+		content="Bild est un outil pédagogique permettant aux enfants ayant des troubles du spectre autistique d'identifier différents objets ou situations "
+	/>
+	<meta property="og:site_name" content="Bild" />
+	<meta property="og:url" content="https://ptitdom.vercel.app/" />
+	<meta property="og:locale" content="fr" />
+	<meta property="og:image" content="https://metatags.arthaud.dev/sprites/hero-min.jpg" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://ptitdom.vercel.app/" />
+	<meta name="twitter:title" content="Bild, votre jeu de cartes numérique - Bild" />
+	<meta
+		name="twitter:description"
+		content="Bild est un outil pédagogique permettant aux enfants ayant des troubles du spectre autistique d'identifier différents objets ou situations "
+	/>
+	<meta name="twitter:image" content="https://metatags.arthaud.dev/sprites/hero-min.jpg" />
+
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-title" content="Bild, votre jeu de cartes numérique" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="#74DAD5" />
+	<meta name="theme-color" content="#74DAD5" />
+</svelte:head>
 
 <main class=" px-8 py-28 md:px-24 relative">
 	<img src="/images/stain/up-left-yellow-form-v2.png" alt="" class="absolute top-0 left-0 -z-10" />
