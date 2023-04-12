3aff3288f638a36f7d9e9d8443dcf8f0f6a246f4
Héloise <heloise.pingitore@mmibordeaux.com>
2023-04-12 10:41:34 +0200

    Style choose folder

--- src/routes/folders/+page.svelte
+++ src/routes/folders/+page.svelte
@@ -1,3 +1,25 @@
-<p>page folders</p>
+<script>
+	import CardsGrid from '$lib/components/CardsGrid.svelte';
+</script>
 
-<a href="/folders/folder1">let's go to folder one 🤯</a>
+<section class="m-[10%] mt-20">
+	<h2 class="decoration-black text-5xl font-patrick font-bold mt-6 mb-8">Mes dossiers</h2>
+	<div class="flex flex-row gap-4">
+		<img src="/images/icons/light.svg" alt="" />
+		<p class="decoration-black text-xl font-patrick inline-block">
+			Cliquez sur un dossier pour l’ouvrir et y ajouter des images !
+		</p>
+	</div>
+	<CardsGrid classes="mt-28 gap-28">
+		<a href="/folders/folder1"
+			><img src="/images/folder.svg" alt="" />
+			<p class="decoration-black text-2xl font-patrick inline-block text-center mt-9">
+				Mon Dossier n°1
+			</p>
+		</a>
+		<a href=""
+			><img src="/images/addfolder.svg" alt="" />
+			<p class="decoration-black text-2xl font-patrick inline-block mt-9">Ajouter un dossier</p></a
+		>
+	</CardsGrid>
+</section>
