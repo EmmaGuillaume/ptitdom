35b1e23b03a64c9a520caa34d3c9a0909504e231
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-04-12 09:57:24 +0200

    fix menu

--- src/lib/components/MenuNavBar.svelte
+++ src/lib/components/MenuNavBar.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <section
-	class="px-4 flex bottom-0 flex-row w-screen z-50 justify-center gap-4 fixed md:right-0 md:h-screen md:w-32 md:py-8 bg-lightblue md:flex-col md:items-center md:justify-between md:rounded-l-3xl"
+	class="px-4 flex bottom-0 h-18 flex-row w-screen z-50 justify-center gap-4 fixed md:right-0 md:h-screen md:w-32 md:py-8 bg-lightblue md:flex-col md:items-center md:justify-between md:rounded-l-3xl"
 >
 	<!-- <a href="/play">Commencer le jeu</a> -->
-	<div class="flex flex-row gap-4 md:flex-col items-center md:gap-0">
+	<div class="flex flex-row justify-bottom gap-4 md:flex-col md:items-center md:gap-0">
 		<ActionsBar
 			classes="flex w-fit !mb-0 !mx-0 !static !px-0 !py-2 !bg-lightblue !filter-none hover"
 		>
@@ -51,7 +51,7 @@
 			</Button>
 		</ActionsBar>
 	</div>
-	<div>
+	<div class="flex flex-row justify-bottom">
 		<ActionsBar classes="flex w-fit !mb-0 !mx-0 !static !px-0 !py-2 !bg-lightblue !filter-none">
 			<Button
 				classes="!py-0 !px-0 !m-0"
