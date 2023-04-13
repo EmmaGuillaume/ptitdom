95024df1a3c3da16c70b5046668173b549ca4722
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-04-13 21:17:53 +0200

    filter comme avant

--- src/lib/components/Library.svelte
+++ src/lib/components/Library.svelte
@@ -6,29 +6,18 @@
 	import LibraryTheme from './LibraryTheme.svelte';
 
 	let filter = '';
-	let showButtons = false;
 
 	function handleClick(themeName: string) {
 		filter = filter === themeName ? '' : themeName;
-	}
-
-	function functionShowButtons() {
-		showButtons = !showButtons;
 	}
 </script>
 
 <Section>
 	<H1>Bibliothèque d’images</H1>
 
-	<button class="btnShowButtons" on:click={() => functionShowButtons()}>+ Ajouter un filtre</button>
-
-	{#if showButtons}
-		<div class="divThemes">
-			{#each themes as theme}
-				<button on:click={() => handleClick(theme.name)}>{theme.name}</button>
-			{/each}
-		</div>
-	{/if}
+	{#each themes as theme}
+		<button on:click={() => handleClick(theme.name)}>{theme.name}</button>
+	{/each}
 
 	{#each themes as theme}
 		{#if !filter || filter === theme.name}
@@ -43,26 +32,7 @@
 </Section>
 
 <style>
-
-.btnShowButtons {
-	padding-left: 5px;
-	padding-right: 5px;
-	background: #F8F8F8;
-	border-radius: 5px;
+button {
+	margin: 10px;
 }
-
-.divThemes {
-	position: absolute;
-	transform: translateY(15px);
-	display: flex;
-	flex-direction: column;
-	align-items: start;
-	padding-left: 5px;
-	padding-right: 5px;
-	background: #F8F8F8;
-	border-radius: 5px;
-	z-index: 100;
-	box-shadow: 0px 0px 10px -5px;
-}
-
 </style>
