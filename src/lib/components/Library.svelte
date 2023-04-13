533ee70d285a5e20fbc5cf8c793937e1cb60ba80
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-04-13 11:45:07 +0200

    librairie v1

--- src/lib/components/Library.svelte
+++ src/lib/components/Library.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
 	import Section from './Section.svelte';
 
-	import { categories } from '$lib/data/categories';
-	import CardsGrid from './CardsGrid.svelte';
-	import ImgButton from './ImgButton.svelte';
+	import { themes } from '$lib/data/themes';
+	import H1 from './H1.svelte';
+	import H2 from './H2.svelte';
+	import LibraryTheme from './LibraryTheme.svelte';
+
 </script>
 
 <Section>
-	<p>library</p>
-	{#each categories as categorie}
-		<p>{categorie.name}</p>
-		<CardsGrid>
-			{#each categorie.images as image}
-				<ImgButton {image} />
+	<H1>Bibliothèque d’images</H1>
+
+	{#each themes as theme}
+		<button>{theme.name}</button>
+	{/each}
+
+	{#each themes as theme}
+		<div id="{theme.name}">
+			<H2>{theme.name}</H2>
+			{#each theme.categoriesName as categorieName}
+				<LibraryTheme categorieName={categorieName.name} />
 			{/each}
-		</CardsGrid>
+		</div>
 	{/each}
 </Section>
---
+++ src/lib/components/LibraryTheme.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { categories, type TCategorie } from '$lib/data/categories';
+	import type { TThemes } from '$lib/data/themes';
+	import CardsGrid from './CardsGrid.svelte';
+
+	import H3 from './H3.svelte';
+	import ImgButton from './ImgButton.svelte';
+
+	export let categorieName: string;
+
+	let categoriesNames = categories.find((categorie) => {
+		return categorie.name === categorieName;
+	}) as TCategorie;
+
+</script>
+
+<div id="{categoriesNames.name}">
+    <H3>{categoriesNames.name}</H3>
+    <CardsGrid>
+        {#each categoriesNames.images as image}
+            <ImgButton {image} />
+        {/each}
+    </CardsGrid>
+</div>
