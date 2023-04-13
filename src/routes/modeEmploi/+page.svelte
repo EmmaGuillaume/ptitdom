003f5247998b7af80efb7182909b00ec5b7730b6
Héloise <heloise.pingitore@mmibordeaux.com>
2023-04-13 21:53:35 +0200

    correction ex

--- src/routes/modeEmploi/+page.svelte
+++ src/routes/modeEmploi/+page.svelte
@@ -3,45 +3,40 @@
 	import Section from '$lib/components/Section.svelte';
 </script>
 
-<Section>
-	<img src="/images/stain/up-left-yellow-form.png" alt="" class="-z-10 absolute left-0 top-0" />
-	<img src="/images/stain/2.png" alt="" class="-z-10 absolute right-0 top-0" />
-	<img
-		src="/images/stain/down-right-green-form.png"
-		alt=""
-		class="-z-10 absolute right-0 bottom-0"
-	/>
-	<div class="md:ml-12">
-		<BackButton href="../" />
+<img src="/images/stain/up-left-yellow-form.png" alt="" class="-z-10 absolute left-0 top-0" />
+<img src="/images/stain/2.png" alt="" class="-z-10 absolute right-0 top-0" />
+<img src="/images/stain/down-right-green-form.png" alt="" class="-z-10 absolute right-0 bottom-0" />
+<div class="md:ml-12">
+	<BackButton href="../" />
+</div>
+<h1 class="mt-8 md:mt-12 decoration-black sm:text-3xl text-2xl font-comforta-regular font-bold">
+	Mode d’emploi :
+</h1>
+<p class="my-8 text-xl">
+	Bild a pour but d’aider les enfants ayant des troubles du spectre autistique à identifier
+	différents objets.
+</p>
+<h2 class="text-2xl mb-4">Étapes du jeu</h2>
+<div class="relative">
+	<div class="w-full mb-4 h-2 md:absolute bg-pink md:w-2 md:mb-0 md:h-full rounded-2xl" />
+	<div class="ml-16 mb-16">
+		<h3 class="my-4 text-xl">
+			1) Sélection des thèmes, catégories et images par les parents ou le psychologue
+		</h3>
+		<img src="/tuto/selection-category1.png" alt="" class="w-6/12" />
+		<img src="/tuto/selection-images1.png" alt="" class="w-6/12" />
+		<h3 class="my-4 text-xl">2) Peaufinage de la sélection dans la galerie</h3>
+		<img src="/tuto/images-choisies1.png" alt="" class="w-6/12" />
+		<h3 class="my-4 text-xl">3) Début de la phase de jeu</h3>
+		<img src="/tuto/jeu.png" alt="" class="w-6/12" />
+		<h3 class="my-4 text-xl">
+			4) Interrogation simple à l’enfant sur les images sélectionnées, Ex : “Montre moi le chat
+			endormi”/ “Où sont les pompiers ?”
+		</h3>
 	</div>
-	<h1 class="mt-8 md:mt-12 decoration-black sm:text-3xl text-2xl font-comforta-regular font-bold">
-		Mode d’emploi :
-	</h1>
-	<p class="my-8 text-xl">
-		Bild a pour but d’aider les enfants ayant des troubles du spectre autistique à identifier
-		différents objets.
-	</p>
-	<h2 class="text-2xl mb-4">Étapes du jeu</h2>
-	<div class="relative">
-		<div class="w-full mb-4 h-2 md:absolute bg-pink md:w-2 md:mb-0 md:h-full rounded-2xl" />
-		<div class="ml-16 mb-16">
-			<h3 class="my-4 text-xl">
-				1) Sélection des thèmes, catégories et images par les parents ou le psychologue
-			</h3>
-			<img src="/tuto/selection-category1.png" alt="" class="w-6/12" />
-			<img src="/tuto/selection-images1.png" alt="" class="w-6/12" />
-			<h3 class="my-4 text-xl">2) Peaufinage de la sélection dans la galerie</h3>
-			<img src="/tuto/images-choisies1.png" alt="" class="w-6/12" />
-			<h3 class="my-4 text-xl">3) Début de la phase de jeu</h3>
-			<img src="/tuto/jeu.png" alt="" class="w-6/12" />
-			<h3 class="my-4 text-xl">
-				4) Interrogation simple à l’enfant sur les images sélectionnées, Ex : “Montre moi le chat
-				endormi”/ “Où sont les pompiers ?”
-			</h3>
-		</div>
-	</div>
+</div>
 
-	<!-- <div class="relative md:ml-12">
+<!-- <div class="relative md:ml-12">
 		<div class="w-full mb-4 h-2 md:absolute bg-pink md:w-2 md:h-full rounded-2xl" />
 		<p class="md:ml-8 text-base font-comforta-regular sm: sm: sm:">
 			Voici le mode d’emploi du site. <br />
@@ -66,4 +61,3 @@
 			reconnecterez sur le site. <br /> <br />
 		</p>
 	</div> -->
-</Section>
